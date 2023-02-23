385b670d43006a892eb8597e7c8f22cac7361596
BarwSirati <sirati10546@gmail.com>
2023-02-23 19:12:06 +0700

    fix : code

--- exercise005/src/App.js
+++ exercise005/src/App.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-=======
 import React from "react";
 import Box from "./components/Box";
 const App = () => {
@@ -60,6 +35,5 @@ const App = () => {
     </div>
   );
 };
->>>>>>> a413a71572400021a06526546990fdf8b23addeb
 
 export default App;
